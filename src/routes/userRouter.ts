1059f66ddb34b19e17be539b3384f9b1107b38b5
number6823 <ohdehet@naver.com>
2026-06-15 15:01:55 +0900

    수정

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -6,13 +6,14 @@ import { loginSchema } from "../schemas/user/login.ts";
 import { authenticate } from "../middlewares/auth.ts";
 import { updateUserSchema } from "../schemas/user/updateUserSchema.ts";
 import { WithdrawUserSchema } from "../schemas/user/withdrawUserSchema.ts";
+import { updatePasswordSchema } from "../schemas/user/updatePasswordSchema.ts";
 
 const router = Router();
 
 router.post("/create", validate(createUserSchema), userController.createUser);
 router.post("/login", validate(loginSchema), userController.login);
 router.patch("/update", authenticate,validate(updateUserSchema), userController.updateUser);
-router.patch("/password", authenticate,validate(updateUserSchema),userController.updatePassword)
+router.patch("/password", authenticate,validate(updatePasswordSchema),userController.updatePassword)
 router.delete("/withdraw",authenticate,validate(WithdrawUserSchema),userController.withdrawUser )
 
 
